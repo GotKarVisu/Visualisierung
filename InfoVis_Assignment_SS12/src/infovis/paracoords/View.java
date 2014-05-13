5c2c6cc395ec12f5e7dd04039dfb56de0fe74304
Stechpalme <andre.karge@gmail.com>
2014-05-13 18:15:15 +0200

    parallel coords funzen :)

--- InfoVis_Assignment_SS12/src/infovis/paracoords/View.java
+++ InfoVis_Assignment_SS12/src/infovis/paracoords/View.java
@@ -1,11 +1,13 @@
 package infovis.paracoords;
 
 import infovis.debug.Debug;
+import infovis.scatterplot.Data;
 import infovis.scatterplot.Model;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
@@ -21,10 +23,11 @@ public class View extends JPanel {
 		g2D.clearRect(0, 0, getWidth(), getHeight());
 		drawCoordinateSystem(g2D);
 		drawLables(g2D);
+		drawData(g2D);
 	}
 	public void drawCoordinateSystem(Graphics2D g2D) {
 		int coordWidth = (getWidth()-padding)/model.getDim();
-		for(int i=0; i<model.getDim(); i++) {
+		for(int i=0; i<model.getDim(); ++i) {
 			int verschiebung = padding + i * coordWidth;
 			g2D.drawLine(verschiebung, padding, verschiebung, getHeight()-padding);
 		}
@@ -38,7 +41,33 @@ public class View extends JPanel {
 			i++;
 		}
 	}
-
+	public void drawData(Graphics2D g2D) {
+		//Schleife über Alle Objekte
+		int coordWidth = (getWidth()-padding)/model.getDim();
+		for(int i=0; i<model.getList().size(); ++i) {
+			Data d = model.getList().get(i);
+			//Schleife über alle Values eines Objekts
+			for(int j=0; j<model.getDim()-1; ++j) {
+				double max = padding;
+				double min = getHeight()-padding;
+				
+				double valLeft = d.getValue(j);
+				double maxLeft = model.getRanges().get(j).getMax();
+				double minLeft = model.getRanges().get(j).getMin();
+				double prozLeft = (100/(maxLeft-minLeft))*(valLeft-minLeft);
+				int coordYLeft = (int) ((((max-min)/100)*prozLeft)+min);
+				
+				double valRight = d.getValue(j+1);
+				double maxRight = model.getRanges().get(j+1).getMax();
+				double minRight = model.getRanges().get(j+1).getMin();
+				double prozRight = (100/(maxRight-minRight))*(valRight-minRight);
+				int coordYRight = (int) ((((max-min)/100)*prozRight)+min);
+				
+				g2D.drawLine(padding+j*coordWidth, coordYLeft, padding+(j+1)*coordWidth, coordYRight);
+			}
+		}
+//		Debug.println(String.valueOf(model.getList().size()));
+	}
 	@Override
 	public void update(Graphics g) {
 		paint(g);
