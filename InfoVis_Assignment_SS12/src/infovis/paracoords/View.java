03b44ede9ac08c8c4a39b724228376b480c63893
Stechpalme <andre.karge@gmail.com>
2014-05-13 17:09:39 +0200

    added lables

--- InfoVis_Assignment_SS12/src/infovis/paracoords/View.java
+++ InfoVis_Assignment_SS12/src/infovis/paracoords/View.java
@@ -11,8 +11,8 @@ import javax.swing.JPanel;
 
 public class View extends JPanel {
 	private Model model = null;
-	private int padding = 50;
-
+	private int padding = 80;
+	
 	@Override
 	public void paint(Graphics g) {
 		
@@ -20,17 +20,23 @@ public class View extends JPanel {
 		g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
 		g2D.clearRect(0, 0, getWidth(), getHeight());
 		drawCoordinateSystem(g2D);
+		drawLables(g2D);
 	}
 	public void drawCoordinateSystem(Graphics2D g2D) {
-//		g2D.drawLine(padding, padding, padding, getHeight()-padding);
-//		g2D.drawLine(getWidth()-padding, padding, getWidth()-padding, getHeight()-padding);
-//		Debug.println(String.valueOf(model.getDim()));
-		for(int i=0; i<model.getDim(); ++i) {
-			int verschiebung = padding + i * (getWidth())/model.getDim();
+		int coordWidth = (getWidth()-padding)/model.getDim();
+		for(int i=0; i<model.getDim(); i++) {
+			int verschiebung = padding + i * coordWidth;
 			g2D.drawLine(verschiebung, padding, verschiebung, getHeight()-padding);
 		}
-		
-		g2D.drawLine(padding, getHeight()-padding, model.getDim()*(getWidth()/model.getDim()), getHeight()-padding);
+		g2D.drawLine(padding, getHeight()-padding, padding+(model.getDim()-1)*coordWidth, getHeight()-padding);
+	}
+	public void drawLables(Graphics2D g2D) {
+		int coordWidth = (getWidth()-padding)/model.getDim();
+		int i = 0;
+		for (String l : model.getLabels()) {
+			g2D.drawString(l, (int) (padding+coordWidth*i+3), (int) (getHeight()-padding+13)+13*(i%2));
+			i++;
+		}
 	}
 
 	@Override
